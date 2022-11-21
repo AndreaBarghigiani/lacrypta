a15a464f07ad8f803fb47855d83c48ce99e51718
Andrea Barghigiani <a.barghigiani@gmail.com>
2022-11-21 12:40:30 +0100

    feat: hero background

--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,20 @@ import PostsList from "../components/postsList";
 export default function Home({ posts }) {
   return (
     <>
-      <div className="py-6 hero bg-base-200 min-h-fit">
-        <div className="text-center hero-content">
+      <div
+        className="rounded-md hero bg-base-200 min-h-max"
+        style={{
+          backgroundImage: `url('images/hero-geroglifici.jpg')`,
+          backgroundSize: `contain`,
+        }}
+      >
+        <div className="hero-overlay bg-opacity-80"></div>
+        <div className="py-10 text-center hero-content">
           <div className="max-w-md">
-            <h1 className="text-5xl font-bold">La Crypta</h1>
-            <p className="py-6">
+            <h1 className="text-5xl font-bold text-neutral-content">
+              La Crypta
+            </h1>
+            <p className="py-6 text-neutral-content">
               Benvenuti nel portale dove si tratta l&apos;argomento crytovalute
               con serenit&aacute; e professionalit&aacute;.
             </p>
