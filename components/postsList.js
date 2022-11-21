8d29ee517b4bd515a779d04a0f453d490a5a8a00
Andrea Barghigiani <a.barghigiani@gmail.com>
2022-11-21 05:27:07 +0100

    fix: missing key

--- components/postsList.js
+++ components/postsList.js
@@ -5,7 +5,7 @@ export default function PostsList({ posts }) {
     <div className="max-w-2xl py-8 mx-auto space-x-3">
       <h2 className="mb-3 text-3xl font-bold text-center">Ultimi articoli</h2>
       {posts.map(({ slug, frontmatter }) => (
-        <Post slug={slug} frontmatter={frontmatter} />
+        <Post key={slug} slug={slug} frontmatter={frontmatter} />
       ))}
     </div>
   );
