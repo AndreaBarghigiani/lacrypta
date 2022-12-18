b1c1f329697e1720c02d169f5dbd83f02add2c4b
Andrea Barghigiani <a.barghigiani@gmail.com>
2022-12-18 18:15:36 +0100

    fix: date sorted posts

--- components/postsList.js
+++ components/postsList.js
@@ -1,10 +1,13 @@
 import Post from "./post";
 
 export default function PostsList({ posts }) {
+  const sortedPosts = posts.sort((a, b) =>
+    a.frontmatter.date > b.frontmatter.date ? -1 : 1
+  );
   return (
     <div className="max-w-2xl py-8 mx-auto space-x-3">
       <h2 className="mb-3 text-3xl font-bold text-center">Ultimi articoli</h2>
-      {posts.map(({ slug, frontmatter }) => (
+      {sortedPosts.map(({ slug, frontmatter }) => (
         <Post key={slug} slug={slug} frontmatter={frontmatter} />
       ))}
     </div>
