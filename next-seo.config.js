30f690fade65ee15914e1b1b8a0b5eedef31871f
Andrea Barghigiani <a.barghigiani@gmail.com>
2022-12-26 08:03:05 +0100

    feat: base setting for index and post

---
+++ next-seo.config.js
@@ -0,0 +1,13 @@
+export default {
+  title: "Cresciamo in DeFi",
+  titleTemplate: "%s | La Crypta",
+  openGraph: {
+    type: "website",
+    locale: "it_IT",
+    url: "https://lacrypta.io/",
+    siteName: "La Crypta | La community italiana sulla DeFi",
+  },
+  twitter: {
+    handle: "@La__Crypta",
+  },
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
+import { DefaultSeo } from "next-seo";
+
 import "../styles/globals.css";
+import SEO from "../next-seo.config";
 
 import Layout from "../components/layout";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </Layout>
   );
